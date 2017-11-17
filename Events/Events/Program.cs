71778744bc19d37e2ce3a36558cbfeb053576a1d
Vasily Marchenko <vam@sitecore.net>
2017-11-18 01:33:16 +0200

    Simplify events

--- Events/Events/Program.cs
+++ Events/Events/Program.cs
@@ -8,17 +8,23 @@ namespace Events
         {
             BankAccount account = new BankAccount(200);
 
-            account.MinusEvent += new BankAccount.MinusHandler(ShowMinusMessage1);
-            account.MinusEvent += new BankAccount.MinusHandler(ShowMinusMessage2);
+            account.MinusEvent += ShowMinusMessage1;
+            account.MinusEvent += ShowMinusMessage2;
+            //account.MinusEvent += AccountOnMinusEvent;
 
-            account.ZeroEvent += new BankAccount.ZeroHandler(ShowZeroMessage1);
-            account.ZeroEvent += new BankAccount.ZeroHandler(ShowZeroMessage2);
+            account.ZeroEvent += ShowZeroMessage1;
+            account.ZeroEvent += ShowZeroMessage2;
 
             account.GetMoney(200);
             account.GetMoney(128);
 
             Console.ReadLine();
         }
+
+        //private static void AccountOnMinusEvent(string msg)
+        //{
+        //    throw new NotImplementedException();
+        //}
 
         public static void ShowMinusMessage1(string text)
         {
