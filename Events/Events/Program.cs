356d6df781d0b72549e38c4f92a6b20cbfb0295e
Vasily Marchenko <vam@sitecore.net>
2017-11-18 03:06:18 +0200

    Introduced Lamdas

--- Events/Events/Program.cs
+++ Events/Events/Program.cs
@@ -8,20 +8,20 @@ namespace Events
         {
             BankAccount account = new BankAccount(200);
 
-            account.MinusEvent += delegate(object sender, BankEventArgs eventArgs)
+            account.MinusEvent += (sender, eventArgs) =>
             {
                 Console.WriteLine($"ShowMinusMessage1: {eventArgs.Message}, Rest={eventArgs.Rest}, OperationSum={eventArgs.OperationSum}");
             };
-            account.MinusEvent += delegate(object sender, BankEventArgs eventArgs)
+            account.MinusEvent += (sender, eventArgs) =>
             {
                 Console.WriteLine($"ShowMinusMessage2: {eventArgs.Message}, Rest={eventArgs.Rest}, OperationSum={eventArgs.OperationSum}");
             };
 
-            account.ZeroEvent += delegate(object sender, BankEventArgs eventArgs)
+            account.ZeroEvent += (sender, eventArgs) =>
             {
                 Console.WriteLine($"ShowZeroMessage1: {eventArgs.Message}, Rest={eventArgs.Rest}, OperationSum={eventArgs.OperationSum}");
             };
-            account.ZeroEvent += delegate(object sender, BankEventArgs eventArgs)
+            account.ZeroEvent += (sender, eventArgs) =>
             {
                 Console.WriteLine($"ShowZeroMessage1: {eventArgs.Message}, Rest={eventArgs.Rest}, OperationSum={eventArgs.OperationSum}");
             };
