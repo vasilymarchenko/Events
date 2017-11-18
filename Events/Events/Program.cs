1f3c9ea39db30cdcd5d04e7c4136345b643235b8
Vasily Marchenko <vam@sitecore.net>
2017-11-18 02:44:54 +0200

    Introduced Anonimous methods

--- Events/Events/Program.cs
+++ Events/Events/Program.cs
@@ -8,36 +8,28 @@ namespace Events
         {
             BankAccount account = new BankAccount(200);
 
-            account.MinusEvent += ShowMinusMessage1;
-            account.MinusEvent += ShowMinusMessage2;
+            account.MinusEvent += delegate(object sender, BankEventArgs eventArgs)
+            {
+                Console.WriteLine($"ShowMinusMessage1: {eventArgs.Message}, Rest={eventArgs.Rest}, OperationSum={eventArgs.OperationSum}");
+            };
+            account.MinusEvent += delegate(object sender, BankEventArgs eventArgs)
+            {
+                Console.WriteLine($"ShowMinusMessage2: {eventArgs.Message}, Rest={eventArgs.Rest}, OperationSum={eventArgs.OperationSum}");
+            };
 
-            account.ZeroEvent += ShowZeroMessage1;
-            account.ZeroEvent += ShowZeroMessage2;
+            account.ZeroEvent += delegate(object sender, BankEventArgs eventArgs)
+            {
+                Console.WriteLine($"ShowZeroMessage1: {eventArgs.Message}, Rest={eventArgs.Rest}, OperationSum={eventArgs.OperationSum}");
+            };
+            account.ZeroEvent += delegate(object sender, BankEventArgs eventArgs)
+            {
+                Console.WriteLine($"ShowZeroMessage1: {eventArgs.Message}, Rest={eventArgs.Rest}, OperationSum={eventArgs.OperationSum}");
+            };
 
             account.GetMoney(200);
             account.GetMoney(128);
 
             Console.ReadLine();
-        }
-
-        public static void ShowMinusMessage1(object sender, BankEventArgs args)
-        {
-            Console.WriteLine($"ShowMinusMessage1: {args.Message}, Rest={args.Rest}, OperationSum={args.OperationSum}");
-        }
-
-        public static void ShowMinusMessage2(object sender, BankEventArgs args)
-        {
-            Console.WriteLine($"ShowMinusMessage1: {args.Message}, Rest={args.Rest}, OperationSum={args.OperationSum}");
-        }
-
-        public static void ShowZeroMessage1(object sender, BankEventArgs args)
-        {
-            Console.WriteLine($"ShowZeroMessage1: {args.Message}, Rest={args.Rest}, OperationSum={args.OperationSum}");
-        }
-
-        public static void ShowZeroMessage2(object sender, BankEventArgs args)
-        {
-            Console.WriteLine($"ShowZeroMessage1: {args.Message}, Rest={args.Rest}, OperationSum={args.OperationSum}");
         }
     }
 }
